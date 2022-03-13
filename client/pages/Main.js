a7e73c453e256315033affd4a7307651ffd4b1b4
Ivan Tsyganov <vatergo@yandex-team.ru>
2022-03-13 19:32:59 +0300

    fix: height phone

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles(() => ({
     justifyContent: "space-around",
     alignItems: "center",
     width: "100%",
-    height: "90%",
+    height: "85%",
   },
   startButton: {
     height: 110,
